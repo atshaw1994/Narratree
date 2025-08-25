695c886bacab796d56c72e26d880ef3798b81732
atshaw1994 <atshaw1994@gmail.com>
2025-08-25 12:02:38 -0400

    Implement article authoring

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_24_173815) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_25_155802) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
