63e8c9ab6c2f7f430eec26ad965e004e4723682e
atshaw1994 <atshaw1994@gmail.com>
2025-08-30 23:05:38 -0400

    Migrate saved_articles to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_27_231725) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_31_030426) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -31,15 +31,27 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_231725) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_likes_on_article_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "saved_articles", default: "[]"
   end
 
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "articles"
+  add_foreign_key "likes", "users"
 end
