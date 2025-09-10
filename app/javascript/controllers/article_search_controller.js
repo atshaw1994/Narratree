cccc681765d8564e352da9007aca1283739e71d9
atshaw1994 <atshaw1994@gmail.com>
2025-09-10 01:49:10 -0400

    Switch to PostgreSQL

--- app/javascript/controllers/article_search_controller.js
+++ app/javascript/controllers/article_search_controller.js
@@ -1,19 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["input", "articlesContainer", "noArticles"]
+
   connect() {
-    this.articles = document.querySelectorAll(".article-card-list-item");
+    this.articles = this.articlesContainerTarget.querySelectorAll(".article-card-list-item");
   }
 
-  filter(event) {
-    const query = event.target.value.toLowerCase();
+  filter() {
+    const query = this.inputTarget.value.toLowerCase();
+    let hasResults = false;
+
     this.articles.forEach(article => {
       const title = article.dataset.title;
       if (title.includes(query)) {
         article.style.display = ""; // Show the article
+        hasResults = true;
       } else {
         article.style.display = "none"; // Hide the article
       }
     });
+
+    if (hasResults) {
+      this.noArticlesTarget.style.display = "none";
+    } else {
+      this.noArticlesTarget.style.display = "";
+    }
   }
 }
