04c11aa16e608c2ea172179cb288b4d26bf7ce8f
atshaw1994 <atshaw1994@gmail.com>
2025-09-12 11:46:38 -0400

    Hamburger Menu for mobile viewing

--- app/javascript/controllers/login_modal_controller.js
+++ app/javascript/controllers/login_modal_controller.js
@@ -1,14 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
+console.log("Login Modal Controller Loaded");
+
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["modal"];
 
   show(event) {
-    event.preventDefault()
-    this.modalTarget.classList.add("visible")
+    event.preventDefault();
+    console.log("Showing login-modal...");
+    this.modalTarget.classList.add("visible");
+    // Hide sidebar if open
+    const sidebar = document.querySelector('.mobile-sidebar.sidebar-visible');
+    if (sidebar) {
+      sidebar.classList.remove('sidebar-visible');
+    }
   }
 
   hide() {
-    this.modalTarget.classList.remove("visible")
+    this.modalTarget.classList.remove("visible");
   }
 }
