0fb66dbde0cfc86cbc806b9a0b75abb7b38c1c87
atshaw1994 <atshaw1994@gmail.com>
2025-09-18 02:04:17 -0400

    Pending User email and active storage patch update

--- config/initializers/active_storage_patch.rb
+++ config/initializers/active_storage_patch.rb
@@ -1,16 +1,28 @@
-# Patch Active Storage S3Service to disable checksum header for Cloudflare R2 compatibility
 Rails.application.config.to_prepare do
   if defined?(ActiveStorage::Service::S3Service)
     ActiveStorage::Service::S3Service.class_eval do
       private
 
-      def upload_with_checksum(key, io, checksum: nil, **options)
+      def upload(key, io, checksum: nil, **options)
         instrument :upload, key: key, checksum: checksum do
-          object_for(key).put(body: io, **options.except(:checksum))
+          options.delete(:checksum_sha256)
+          options.delete(:checksum_sha1)
+          options.delete(:checksum_crc32)
+          options.delete(:checksum_crc32c)
+          object_for(key).put(body: io, **options)
         end
       end
 
-      alias_method :upload, :upload_with_checksum
+      def upload_without_unfurling(key, io, checksum: nil, **options)
+        instrument :upload, key: key, checksum: checksum do
+          options.delete(:checksum_sha256)
+          options.delete(:checksum_sha1)
+          options.delete(:checksum_crc32)
+          options.delete(:checksum_crc32c)
+          object_for(key).put(body: io, **options)
+        end
+      end
     end
   end
+  Rails.logger.info "ActiveStorage S3Service patch loaded" if defined?(Rails)
 end
