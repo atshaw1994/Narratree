aa1b3469652fd27a8b007e471598424371d8fc42
atshaw1994 <atshaw1994@gmail.com>
2025-09-20 13:05:05 -0400

    Fix CSRF token

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_narratree_session"
+Rails.application.config.session_store :cookie_store,
+  key: "_narratree_session",
+  secure: Rails.env.production?,
+  same_site: :lax,
+  domain: (Rails.env.production? ? [ "narratree.onrender.com", "narratree.net" ] : nil)
