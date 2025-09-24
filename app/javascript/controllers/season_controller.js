b7d04a5b2c7464b952a9abd2450df44fc4f1054f
atshaw1994 <atshaw1994@gmail.com>
2025-09-24 10:19:47 -0400

    Update Season Controller

--- app/javascript/controllers/season_controller.js
+++ app/javascript/controllers/season_controller.js
@@ -1,42 +1,41 @@
 import { Controller } from "@hotwired/stimulus";
 
-console.log("Season Controller Loaded");
-
 export default class extends Controller {
     connect() {
-        console.log("Season Controller Connected");
-        this.maxLeaves = 50;
+        this.maxFallingElements = 50;
         this.stopped = false;
-        if (this.isChristmasSeason()) {
-            this.createElements();
-        }
-        else if (this.isFallSeason()) {
-            this.maxLeaves = 30;
-            this.stopped = false;
-            this.createElements();
-            let jackolanternElement = document.createElement("img");
-            let candleElement = document.createElement("div");
-            candleElement.className = "candle-light flicker";
-            const footer = document.querySelector("footer");
-            const jackolanternSrc = footer.dataset.jackolanternSrc;
-            jackolanternElement.src = jackolanternSrc;
-            jackolanternElement.alt = "JackOLantern";
-            jackolanternElement.className = "jackolantern";
+        switch (this.getSeason()) {
+            case "fall":
+                this.maxFallingElements = 30;
+                this.stopped = false;
+                this.createElements("leaf");
+                break;
+            case "spooky":
+                let jackolanternElement = document.createElement("img");
+                let candleElement = document.createElement("div");
+                candleElement.className = "candle-light flicker";
+                const footer = document.querySelector("footer");
+                const jackolanternSrc = footer.dataset.jackolanternSrc;
+                jackolanternElement.src = jackolanternSrc;
+                jackolanternElement.alt = "JackOLantern";
+                jackolanternElement.className = "jackolantern";
 
-            footer.appendChild(jackolanternElement);
-            footer.appendChild(candleElement);
+                footer.appendChild(jackolanternElement);
+                footer.appendChild(candleElement);
+                break;
+            case "christmas":
+                this.maxFallingElements = 30;
+                this.stopped = false;
+                this.createElements("snowflake");
+                break;
+            default:
+                // No seasonal effects
+                break;
         }
     }
 
-    async createElements() {
-        const count = this.maxLeaves;
-        let elementClass = "leaf";
-        if (this.isChristmasSeason()) {
-            elementClass = "snowflake";
-        }
-        else if (this.isFallSeason()) {
-            elementClass = "leaf";
-        }
+    async createElements(elementClass) {
+        const count = this.maxFallingElements;
 
         for (let i = 0; i < count; i++) {
             if (this.stopped) break;
@@ -57,13 +56,17 @@ export default class extends Controller {
         this.element.appendChild(element);
     }
 
-    isChristmasSeason() {
+    getSeason() {
         const month = new Date().getMonth();
-        return month === 11; // December
-    }
-
-    isFallSeason() {
-        const month = new Date().getMonth();
-        return month === 8 || month === 9 || month === 10; // September, October, November
+        const day = new Date().getDate();
+        if (month === 8 || month === 10) {
+            return "fall";
+        } else if (month === 9) {
+            return "spooky";
+        }
+        else if (month === 11) {
+            return "christmas";
+        }
+        return "unknown";
     }
 }
