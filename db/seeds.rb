a148a6f9cca7540a3106cb781b03737c4691d09a
atshaw1994 <atshaw1994@gmail.com>
2025-12-01 07:37:43 -0500

    Fix seeds [skip render]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,21 @@
-# Create owner account if it doesn't exist
-User.find_or_create_by!(email: "atshaw1994@gmail.com") do |user|
-  user.first_name = "Aaron"
-  user.last_name = "Shaw"
-  user.username = "atshaw1994"
-  user.password = "Hazel1021!"
-  user.password_confirmation = "Hazel1021!"
-  user.role = :owner
-  user.accepted_guidelines = true
+# Create owner account if it doesn't exist, with retry workaround for Devise mapping error
+begin
+  User.find_or_create_by!(email: "atshaw1994@gmail.com") do |user|
+    user.first_name = "Aaron"
+    user.last_name = "Shaw"
+    user.username = "atshaw1994"
+    user.password = "Hazel1021!"
+    user.password_confirmation = "Hazel1021!"
+    user.role = "owner"
+    user.accepted_guidelines = true
+  end
+rescue RuntimeError => e
+  if e.message =~ /valid mapping/
+    sleep 1
+    retry
+  else
+    raise
+  end
 end
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
